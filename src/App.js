529b5127ce9e1876bcec3897dcac8f08b512910c
mahimaanchra <mahimaanchra@gmail.com>
2026-08-30 23:32:06 +0530

    feat: setup core application entry point and global styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-function App() {
+import { AuthProvider }  from "./context/AuthContext";
+import ProtectedRoute    from "./components/ProtectedRoute";
+import HomePage          from "./pages/HomePage";
+import CustomerMenu      from "./pages/CustomerMenu";
+import AdminLogin        from "./pages/AdminLogin";
+import AdminMenu         from "./components/AdminMenu";
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Public */}
+          <Route path="/"            element={<HomePage />} />
+          <Route path="/menu"        element={<CustomerMenu />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+
+          {/* Protected admin */}
+          <Route path="/admin" element={
+            <ProtectedRoute>
+              <AdminMenu />
+            </ProtectedRoute>
+          } />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
-
-export default App;
